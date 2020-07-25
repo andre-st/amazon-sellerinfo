e70643c78a2efe7266a6eb32f62cef0d6b73cd88
andre-st <datakadabra@gmail.com>
2020-07-26 01:34:17 +0200

    Tidy up code

--- content.js
+++ content.js
@@ -1,6 +1,7 @@
 const SELLER_RATING_REX      = new RegExp( 'feedback-detail-description" href="#"><b>([0-9]+%)',       'm' );
 const SELLER_COUNTRY_REX     = new RegExp( '<span class="a-list-item">([A-Z]{2})<\/span><\/li><\/ul>', 'm' );    // Two letter code
 const SELLER_URL_ID_REX      = new RegExp( 'seller=([a-zA-Z9-9+-_]+)' );
+const SELLER_LINK_SELECTOR   = 'a[href^="/gp/help/seller/at-a-glance.html"]';
 const SELLER_URL_STUB        = window.location.origin + '/gp/help/seller/at-a-glance.html/ref=ox_sc_seller_sfl_s1?seller=';  // Absolute URL for permission's sake
 const SELLER_RATING_UNKNOWN  = '?';
 const SELLER_COUNTRY_UNKNOWN = '?';
@@ -10,7 +11,7 @@ const SELLER_COUNTRY_UNKNOWN = '?';
 // We don't want multiple requests for basically the same info
 // as it also triggers Amazon's HTTP503 request throttling.
 // So we will build something with the seller ID:
-const sellerLinks   = Array.from( document.querySelectorAll( 'a[href^="/gp/help/seller/at-a-glance.html"]' ));
+const sellerLinks   = Array.from( document.querySelectorAll( SELLER_LINK_SELECTOR ));
 const sellerIds     = sellerLinks.map( l => l.getAttribute( 'href' ).match( SELLER_URL_ID_REX )[1] );
 const sellerIdsUniq = sellerIds.filter( (v,i,a) => a.indexOf( v ) === i );
 
@@ -41,9 +42,7 @@ sellerIdsUniq.forEach( id =>
 		console.log( '[DEBUG] Fetched ' + absUrl );
 		const m = text.match( SELLER_RATING_REX  );
 		const c = text.match( SELLER_COUNTRY_REX );
-		rateSellerLinks( id,
-				m ? m[1] : SELLER_RATING_UNKNOWN,
-				c ? c[1] : SELLER_COUNTRY_UNKNOWN );
+		rateSellerLinks( id, m ? m[1] : SELLER_RATING_UNKNOWN, c ? c[1] : SELLER_COUNTRY_UNKNOWN );
 	});
 });
 
