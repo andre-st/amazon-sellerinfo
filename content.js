1fb15b92be919414c1b4ec8f78f1e96baeeba2cb
andre-st <datakadabra@gmail.com>
2020-07-26 16:43:41 +0200

    Reduced code

--- content.js
+++ content.js
@@ -1,11 +1,9 @@
-const SELLER_RATING_UNKNOWN  = '?';
-const SELLER_COUNTRY_UNKNOWN = '?';
-const SELLER_RATING_REX      = new RegExp( 'feedback-detail-description" href="#"><b>([0-9]+%)',       'm' );
-const SELLER_COUNTRY_REX     = new RegExp( '<span class="a-list-item">([A-Z]{2})<\/span><\/li><\/ul>', 'm' );    // Two letter code
-const SELLER_URL_ID_REX      = new RegExp( 'seller=([a-zA-Z9-9+-_]+)' );
 const SELLER_LINK_SELECTOR   = 'a[href^="/gp/help/seller/at-a-glance.html"]';
-const sellerUrl              = id     => window.location.origin + '/gp/help/seller/at-a-glance.html/ref=ox_sc_seller_sfl_s1?seller=' + id;  // Absolute URL for permission's sake
-const isUrlOfSeller          = (u,id) => u.includes( 'seller=' + id );
+const extractSellerCountry   = (s    ) => (_ = s.match( /<span class="a-list-item">([A-Z]{2})<\/span><\/li><\/ul>/m )) && _[1];  // Two letter code
+const extractSellerRating    = (s    ) => (_ = s.match( /feedback-detail-description" href="#"><b>([0-9]+%)/m       )) && _[1];
+const sellerIdFromUrl        = (u    ) => (_ = u.match( /seller=([a-zA-Z9-9+-_]+)/                                  )) && _[1];
+const isUrlOfSeller          = (u,id ) => u.includes( 'seller=' + id );
+const sellerUrl              = (  id ) => window.location.origin + '/gp/help/seller/at-a-glance.html/ref=ox_sc_seller_sfl_s1?seller=' + id;  // Absolute URL for permission's sake
 
 
 // Seller info URLs are different even for the same seller.
@@ -14,15 +12,15 @@ const isUrlOfSeller          = (u,id) => u.includes( 'seller=' + id );
 // So we will build something with the seller ID:
 const sellerLinks = Array.from( document.querySelectorAll( SELLER_LINK_SELECTOR ));
 const sellerIds   = sellerLinks
-				.map( l => l.getAttribute( 'href' ).match( SELLER_URL_ID_REX )[1] )
+				.map( l => sellerIdFromUrl( l.getAttribute( 'href' )))
 				.filter( (v,i,a) => a.indexOf( v ) === i );  // Unique
 
 
 // UI elements are added via CSS:
 const updateSellerLinks = (id,r,c) => sellerLinks
 				.filter ( l =>  isUrlOfSeller( l.getAttribute( 'href' ), id ))
-				.forEach( l =>{ l.setAttribute( 'data-andrest-rating',  r )
-				                l.setAttribute( 'data-andrest-country', c ); });
+				.forEach( l =>{ l.setAttribute( 'data-andrest-rating',  r || '?' )
+				                l.setAttribute( 'data-andrest-country', c || '?' ); });
 
 
 // Fetch all seller info asynchronously:
@@ -37,15 +35,13 @@ sellerIds.forEach( id =>
 		else
 		{
 			console.log( '[ERROR] Fetching seller info failed: ' + url + ' (HTTP ' + resp.status + ')' );
-			updateSellerLinks( id, SELLER_RATING_UNKNOWN, SELLER_COUNTRY_UNKNOWN );
+			updateSellerLinks( id );
 		}
 	})
 	.then( text =>
 	{
 		console.log( '[DEBUG] Fetched seller info: ' + url );
-		const m = text.match( SELLER_RATING_REX  );
-		const c = text.match( SELLER_COUNTRY_REX );
-		updateSellerLinks( id, m ? m[1] : SELLER_RATING_UNKNOWN, c ? c[1] : SELLER_COUNTRY_UNKNOWN );
+		updateSellerLinks( id, extractSellerRating( text ), extractSellerCountry( text ));
 	});
 });
 
