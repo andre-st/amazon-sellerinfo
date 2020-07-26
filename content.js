af0f9c381b767b536f8ddb8e13c62218e48cc015
andre-st <datakadabra@gmail.com>
2020-07-26 18:10:36 +0200

    Improve readability

--- content.js
+++ content.js
@@ -1,3 +1,9 @@
+// Since the same seller has different URLs on the shopping cart webpage,
+// we extract the seller IDs from these URLs
+// and construct *fewer* seller URLs using these IDs, query them 
+// and update all links of that seller on the webpage.
+// This is to evade Amazon's HTTP 503 request throttling.
+
 const sellerCountryFrom = (s   ) => (_ = s.match( /<span class="a-list-item">([A-Z]{2})<\/span><\/li><\/ul>/m )) && _[1];  // Two letter code
 const sellerRatingFrom  = (s   ) => (_ = s.match( /feedback-detail-description" href="#"><b>([0-9]+%)/m       )) && _[1];
 const sellerIdFrom      = (s   ) => (_ = s.match( /seller=([a-zA-Z9-9+-_]+)/                                  )) && _[1];
@@ -6,24 +12,21 @@ const sellerUrl         = (  id) => window.location.origin + '/gp/help/seller/at
 const sellerLinks       = Array.from( document.querySelectorAll( 'a[href^="/gp/help/seller/at-a-glance.html"]' ));
 const sellerIds         = sellerLinks
                             .map   ( (l    ) => sellerIdFrom( l.getAttribute( 'href' )))
-                            .filter( (v,i,a) => a.indexOf( v ) === i );
-                            // Since the same seller has different URLs,
-                            // we will create seller URLs using these IDs,
-                            // unique to prevent Amazon's HTTP 503 request throttling
+                            .filter( (v,i,a) => a.indexOf( v ) === i );  // Unique
+
 
 const updateSellerLinks = (id,r,c) => sellerLinks
-                                        .filter ( l =>  isUrlOfSeller( l.getAttribute( 'href' ), id ))
-                                        .forEach( l =>{ l.setAttribute( 'data-andrest-rating',  r || '?' )
-                                                        l.setAttribute( 'data-andrest-country', c || '?' ); });
+                            .filter ( l =>  isUrlOfSeller( l.getAttribute( 'href' ), id ))
+                            .forEach( l =>{ l.setAttribute( 'data-andrest-rating',  r || '?' )
+                                            l.setAttribute( 'data-andrest-country', c || '?' ); });
+
 sellerIds.forEach( id =>
 {
-	const url = sellerUrl( id );  // Absolute URL for permission's sake
-	fetch( url )  // Async!
+	const url = sellerUrl( id );
+	fetch( url )  // Async! Absolute URL for permission's sake.
 	.then( resp =>
 	{
 		if( resp.ok ) return resp.text();
-		
-		//Internet connection OK but server issues:
 		console.error( '[ERROR] Cannot fetch seller info from ' + url + ': HTTP ' + resp.status );
 		updateSellerLinks( id );
 	})
@@ -32,7 +35,7 @@ sellerIds.forEach( id =>
 		console.log( '[DEBUG] Fetched seller info: ' + url );
 		updateSellerLinks( id, sellerRatingFrom( text ), sellerCountryFrom( text ));
 	})
-	.catch( err =>  // Intenet connection issues:
+	.catch( err =>  // Internet connection issues (not web server):
 	{
 		console.error( '[FATAL] Cannot fetch seller info from ' + url + ': ', err );
 		updateSellerLinks( id );
