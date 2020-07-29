7a07ad5b7262839766bced46aecfff65ed7098d9
andre-st <datakadabra@gmail.com>
2020-07-30 00:32:40 +0200

    Close #3

--- content.js
+++ content.js
@@ -1,3 +1,4 @@
+//
 // 1. On the shopping cart webpage, every seller has a different info URL for each item that he sells.
 //    Querying all of one seller's URLs on the page means N requests for the same info.
 //    It might trigger Amazon's HTTP 503 request throttling earlier than necessary, too.
@@ -9,42 +10,73 @@
 //
 // 2. Style-sheets create the visual elements using the data attributes of the updated links.
 //
-
-const sellerCountryFrom = (s   ) => (_ = s.match( /<span class="a-list-item">([A-Z]{2})<\/span><\/li><\/ul>/m )) && _[1];  // Two letter code
-const sellerRatingFrom  = (s   ) => (_ = s.match( /feedback-detail-description" href="#"><b>([0-9]+%)/m       )) && _[1];
-const sellerIdFrom      = (s   ) => (_ = s.match( /seller=([a-zA-Z9-9+-_]+)/                                  )) && _[1];
-const sellerIdFromLink  = (l   ) => sellerIdFrom( l.getAttribute( 'href' ));
-const isUrlOfSeller     = (s,id) => s.includes( 'seller=' + id );
-const sellerUrl         = (  id) => window.location.origin + '/sp/ref=x_' + Date.now() + '?seller=' + id;
-const sellerLinks       = Array.from( document.querySelectorAll( 'a[href*="seller="]' ));
-const sellerIds         = sellerLinks.map( sellerIdFromLink ).filter( (v,i,a) => a.indexOf( v ) === i );  // Unique
+// 3. Moving shopping cart list items creates new items and
+//    pages of the order process are successively loaded via Javascript (so manifest "matches" doesn't match).
+//    Therefore we regularly check and update seller links.
+//    For the sake of simplicity I just use a timer instead of the MutationObserver.
+//
 
 
-const updateSellerLinks = (id,r,c) => sellerLinks
-                            .filter ( l =>  isUrlOfSeller( l.getAttribute( 'href' ), id ))
-                            .forEach( l =>{ l.setAttribute( 'data-andrest-rating',  r || '?' )
-                                            l.setAttribute( 'data-andrest-country', c || '?' ); });
+const _sellers = {};  // Blackboard  { sellerId: { rating: '', country: '' }, ...}
 
-sellerIds.forEach( id =>
+
+function updateSellers()
 {
-	const url = sellerUrl( id );
-	fetch( url )  // Async! Absolute URL for permission's sake.
-	.then( resp =>
-	{
-		if( resp.ok ) return resp.text();
-		console.error( '[ERROR] Cannot fetch seller info from ' + url + ': HTTP ' + resp.status );
-		updateSellerLinks( id );
-	})
-	.then( text =>
-	{
-		console.log( '[DEBUG] Fetched seller info: ' + url );
-		updateSellerLinks( id, sellerRatingFrom( text ), sellerCountryFrom( text ));
-	})
-	.catch( err =>  // Internet connection issues (not web server):
-	{
-		console.error( '[FATAL] Cannot fetch seller info from ' + url + ': ', err );
-		updateSellerLinks( id );
+	const sellerCountryFrom = (s   ) => (_ = s.match( /<span class="a-list-item">([A-Z]{2})<\/span><\/li><\/ul>/m )) && _[1];  // Two letter code
+	const sellerRatingFrom  = (s   ) => (_ = s.match( /feedback-detail-description" href="#"><b>([0-9]+%)/m       )) && _[1];
+	const sellerIdFrom      = (s   ) => (_ = s.match( /seller=([a-zA-Z9-9+-_]+)/                                  )) && _[1];
+	const sellerIdFromLink  = (l   ) => sellerIdFrom( l.getAttribute( 'href' ));
+	const isUrlOfSeller     = (s,id) => s.includes( 'seller=' + id );
+	const sellerUrl         = (  id) => window.location.origin + '/sp/ref=x_' + Date.now() + '?seller=' + id;
+	const sellerLinks       = Array.from( document.querySelectorAll( 'a[href*="seller="]' ));
+	const sellerIds         = sellerLinks.map( sellerIdFromLink ).filter( (v,i,a) => a.indexOf( v ) === i );  // Unique
+	
+	
+	const updateLinks = () => sellerLinks.forEach( l =>
+	{ 
+		const id = sellerIdFromLink( l );
+		if( !(id in _sellers) || !_sellers[id].loaded ) return;
+		l.setAttribute( 'data-andrest-rating',  _sellers[id].rating  || '?' );
+		l.setAttribute( 'data-andrest-country', _sellers[id].country || '?' );
 	});
-});
+	
+	
+	sellerIds
+	.filter ( id => !(id in _sellers))
+	.forEach( id =>
+	{
+		_sellers[id] = { loaded: false };
+		const url = sellerUrl( id );
+		
+		fetch( url )  // Async! Absolute URL for permission's sake.
+		.then( resp =>
+		{
+			if( resp.ok ) return resp.text();
+			console.error( '[ERROR] Cannot fetch seller info from ' + url + ': HTTP ' + resp.status );
+			_sellers[id].loaded = true;
+			updateLinks();
+		})
+		.then( text =>
+		{
+			console.log( '[DEBUG] Fetched seller info: ' + url );
+			_sellers[id].rating  = sellerRatingFrom ( text );
+			_sellers[id].country = sellerCountryFrom( text );
+			_sellers[id].loaded  = true;
+			updateLinks();
+		})
+		.catch( err =>  // Internet connection issues (not web server):
+		{
+			console.error( '[FATAL] Cannot fetch seller info from ' + url + ': ', err );
+			_sellers[id].loaded = true;
+			updateLinks();
+		});
+	});
+	
+	
+	updateLinks();
+}
 
+
+updateSellers();
+setInterval( updateSellers, 1500 );
 
